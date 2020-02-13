6bea07932cacd8ca23871b10c86cfa11d6d020e2
unknown <e.fraffo@gmail.com>
2020-02-13 16:40:50 -0300

    13/02

--- servidor/controladores/controladorPeliculas.js
+++ servidor/controladores/controladorPeliculas.js
@@ -1,15 +1,25 @@
 var con = require('../lib/conexionbd');
 
 function buscarPeliculas(req, res) {
+    // var orden_array = orden.split("-");
+    // query_params.columna_orden = orden_array[0];
+    // query_params.tipo_orden = orden_array[1];
+    // var query = $.param(query_params);
+    // Request URL: http://localhost:8080/peliculas?pagina=1&titulo=love&cantidad=52&columna_orden=titulo&tipo_orden=ASC
+
     var titulo = req.query.titulo;
+    var columna_orden = req.query.columna_orden;
+    var tipo_orden = req.query.tipo_orden;
+    var pagina = req.query.pagina;
+    // var cantidad = req.query.cantidad;
     // var genero = req.query.genero;
-    // var orden = req.query.orden;
     // var anio = req.query.anio;
+
     if(titulo){
-        var sql = "select * from pelicula where titulo like '" + '%' + titulo + '%' + "'" + 'order by titulo';
+        var sql = "select * from pelicula where titulo like '" + '%' + titulo + '%' + "'" + ' order by ' + columna_orden + ' ' + tipo_orden;
     }
     else{
-        var sql = "select * from pelicula order by titulo limit 10";
+        var sql = "select * from pelicula order by " + columna_orden + ' ' + tipo_orden + ' ' + 'limit ' + pagina + ',' + '10';
     }
     // la funcion de callback se ejecuta una vez que se termine de ejecutar la consulta
     con.query(sql, function(error, resultado, fields) {
@@ -18,20 +28,20 @@ function buscarPeliculas(req, res) {
             return res.status(404).send("Hubo un error en la consulta");
         }
         
-        var sqlCount = "select COUNT(*) from pelicula";
+        // var sqlCount = "select COUNT(*) from pelicula";
 
-        con.query(sqlCount, function(error, resultado) {
-            if (error) {
-                console.log("Hubo un error al calcular la cantidad", error.message);
-                return res.status(404).send("Hubo un error al calcular la cantidad");
-            }  
+        // con.query(sqlCount, function(error, resultado) {
+        //     if (error) {
+        //         console.log("Hubo un error al calcular la cantidad", error.message);
+        //         return res.status(404).send("Hubo un error al calcular la cantidad");
+        //     }  
             
-            var respuesta = {
-                'total': resultado[0]
-            };
+        //     var respuesta = {
+        //         'total': resultado[0]
+        //     };
 
-            res.send(respuesta);
-        });
+        //     res.send(JSON.stringify(respuesta));
+        // });
 
         var respuesta = {
             'peliculas': resultado
