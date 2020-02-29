6e86541c7e9adb42fa08c5c84f3e9ca779984bda
unknown <e.fraffo@gmail.com>
2020-02-29 16:33:57 -0300

    29/02

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,3 +1,18 @@
 select * from genero;
-
 select * from pelicula order by titulo asc limit 1,10;
+
+-- ordenar por 
+select * from pelicula order by titulo asc;
+select * from pelicula order by anio desc;
+select * from pelicula order by puntuacion desc;
+
+-- filtros individuales
+select * from pelicula where titulo like '%love%' order by titulo asc;
+select * from pelicula where anio = 2005 order by titulo asc;
+select * from pelicula where genero_id = 1 order by titulo asc;
+select * from pelicula where genero_id = 1 order by anio desc;
+select * from pelicula where genero_id = 1 order by puntuacion desc;
+
+-- filtros combinados
+select * from pelicula where titulo like '%pirates%' and genero_id = 1 order by titulo asc; -- no funciona en el front
+select * from pelicula where anio = 2006 and genero_id = 1 order by titulo asc; -- no funciona en el front
--- servidor/controladores/controladorPeliculas.js
+++ servidor/controladores/controladorPeliculas.js
@@ -5,22 +5,54 @@ function buscarPeliculas(req, res) {
     // query_params.columna_orden = orden_array[0];
     // query_params.tipo_orden = orden_array[1];
     // var query = $.param(query_params);
-    // Request URL: http://localhost:8080/peliculas?pagina=1&titulo=love&cantidad=52&columna_orden=titulo&tipo_orden=ASC
-
-    var titulo = req.query.titulo;
+// Request URL: http://localhost:8080/peliculas?pagina=1&titulo=harry&genero=1&anio=2000&cantidad=52&columna_orden=titulo&tipo_orden=ASC
+    
     var columna_orden = req.query.columna_orden;
     var tipo_orden = req.query.tipo_orden;
-    var pagina = req.query.pagina;
+    // var pagina = req.query.pagina;
     // var cantidad = req.query.cantidad;
-    // var genero = req.query.genero;
-    // var anio = req.query.anio;
 
-    if(titulo){
-        var sql = "select * from pelicula where titulo like '" + '%' + titulo + '%' + "'" + ' order by ' + columna_orden + ' ' + tipo_orden;
+    var titulo = req.query.titulo;
+    var anio = req.query.anio;
+    var genero = req.query.genero;
+    
+
+    if(titulo && anio && genero){
+        var sql = "select * from pelicula where titulo like '" + '%' + titulo + '%' + "'" + ' and ' + ' anio = ' + anio + ' and ' + 'genero_id = ' + genero + ' order by ' + columna_orden + ' ' + tipo_orden;
     }
     else{
-        var sql = "select * from pelicula order by " + columna_orden + ' ' + tipo_orden + ' ' + 'limit ' + pagina + ',' + '10';
+        if(titulo && anio || titulo && genero || anio && genero){
+            if(titulo && anio){
+                var sql = "select * from pelicula where titulo like '" + '%' + titulo + '%' + "'" + ' and ' + ' anio = ' + anio + ' order by ' + columna_orden + ' ' + tipo_orden;
+            }
+            
+            if(titulo && genero){
+                var sql = "select * from pelicula where titulo like '" + '%' + titulo + '%' + "'" + ' and ' + ' genero_id = ' + genero + ' order by ' + columna_orden + ' ' + tipo_orden;
+            }
+    
+            if(anio && genero){
+                var sql = "select * from pelicula where anio = " + anio + " and " + ' genero_id = ' + genero + ' order by ' + columna_orden + ' ' + tipo_orden;
+            }
+        }
+        else{
+            if(titulo || anio || genero){
+                if(titulo){
+                    var sql = "select * from pelicula where titulo like '" + '%' + titulo + '%' + "'" + ' order by ' + columna_orden + ' ' + tipo_orden;
+                }
+                if(anio){
+                    var sql = "select * from pelicula where anio = " + anio + ' order by ' + columna_orden + ' ' + tipo_orden;
+                }
+                if(genero){
+                    var sql = "select * from pelicula where genero_id = " + genero + ' order by ' + columna_orden + ' ' + tipo_orden;
+                }
+            }
+            else{
+                var sql = "select * from pelicula order by " + columna_orden + ' ' + tipo_orden;
+            }
+        }
     }
+
+    
     // la funcion de callback se ejecuta una vez que se termine de ejecutar la consulta
     con.query(sql, function(error, resultado, fields) {
         if (error) {
@@ -68,14 +100,9 @@ function buscarPeliculas(req, res) {
 
 
 function buscarGeneros(req, res) {
-    var id = req.params.id;
-
-    if(id){
-        var sql = "select * from genero where id = " + id;
-    }
-    else{
-        var sql = "select * from genero";
-    }
+    
+    var sql = "select * from genero";
+    
     con.query(sql, function(error, resultado, fields) {
         if (error) {
             console.log("Hubo un error en la consulta", error.message);
