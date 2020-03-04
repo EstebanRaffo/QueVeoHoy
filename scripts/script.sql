2220998e6d5538e44469ce07f413522b78cecf5b
unknown <e.fraffo@gmail.com>
2020-03-03 21:48:58 -0300

    3/03

--- scripts/script.sql
+++ scripts/script.sql
@@ -40,3 +40,39 @@ show create table pelicula;
   KEY `genero_id` (`genero_id`),
   CONSTRAINT `pelicula_ibfk_1` FOREIGN KEY (`genero_id`) REFERENCES `genero` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=744 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci |
+
+
+CREATE TABLE `que_veo_hoy`.`actor` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(70) NOT NULL,
+  PRIMARY KEY (`id`)
+  );
+
+
+CREATE TABLE `que_veo_hoy`.`actor_pelicula` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `actor_id` INT NOT NULL,
+  `pelicula_id` INT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+alter table actor_pelicula add foreign key (actor_id) references actor(id);
+
+alter table actor_pelicula add foreign key (pelicula_id) references pelicula(id);
+
+show create table actor_pelicula;
++----------------+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+| Table          | Create Table                                                                                                                                                                                                                                                                                                                                                                                                                                                                     |
++----------------+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+| actor_pelicula | CREATE TABLE `actor_pelicula` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `actor_id` int NOT NULL,
+  `pelicula_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `actor_id` (`actor_id`),
+  KEY `pelicula_id` (`pelicula_id`),
+  CONSTRAINT `actor_pelicula_ibfk_1` FOREIGN KEY (`actor_id`) REFERENCES `actor` (`id`),
+  CONSTRAINT `actor_pelicula_ibfk_2` FOREIGN KEY (`pelicula_id`) REFERENCES `pelicula` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci |
+
+
